a6e1679039c8cf25a2be4b88817e8a631776fc5c
Adilzhan Zhanabil <adilzhanzhanabil0705@gmail.com>
2026-04-26 13:28:26 +0500

    feat: init url model

--- internal/model/url.go
+++ internal/model/url.go
@@ -1,1 +1,14 @@
 package model
+
+import "time"
+
+type URL struct {
+	ID          int64  `db:"id" json:"id"`
+	OriginalURL string `db:"original_url" json:"original_url"`
+	ShortCode   string `db:"short_code" json:"short_cocreated_at"`
+	Clicks      int64  `db:"clicks" json:"clicks"`
+
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `db:"updated_at" json:"updated_at"`
+	ExpiresAt *time.Time `db:"expires_at" json:"expires_at,omitempty"`
+}
